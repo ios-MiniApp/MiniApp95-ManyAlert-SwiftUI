416c1f9b6d04b594e7f386d04419c92349a42f44
Kota <maedakoutaa@gmail.com>
2022-09-17 11:14:16 +0900

    Initial Commit

---
+++ MiniApp95-ManyAlert-SwiftUI/MiniApp95_ManyAlert_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp95_ManyAlert_SwiftUIApp.swift
+//  MiniApp95-ManyAlert-SwiftUI
+//
+//  Created by 前田航汰 on 2022/09/17.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp95_ManyAlert_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
